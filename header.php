67ac043b647b5c350375e53c64b89aa4e7c0d5ae
mohamed shames <mohamedhames900@gmail.com>
2023-04-05 07:16:59 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <a href="index.php" class="logo">Products<span>box</span></a>
+    <div class="animation"></div>
+    <nav class="navbar">
+        <a href="Home">home</a>
+        <a href="Home">Products</a>
+        <a href="Home">New Products</a>
+        <a href="Home">contact us</a>
+        <a href="Home">Featured</a>
+    </nav>
+    <div class="icons">
+        <i class="fas fa-user" id="user"></i>
+        <a href="cart.php"><i class="fas fa-shopping-cart"><span>(2)</span></i></a>
+        <i class="fas fa-thin fa-bars" id="menu"></i>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <title>Products</title>
+</head>
+<body>
+    <?php include 'header.php';?>
+
+    <section class="home">
+        <div class="box">
+            <h1>Product classification</h1>
+            <div class="animation-home"></div>
+            <p>Many of these types of product information are regulated to some degree</p>
+            <a href="about.php" class="btn">show more</a>
+        </div>
+    </section>
+</body>
+</html>
